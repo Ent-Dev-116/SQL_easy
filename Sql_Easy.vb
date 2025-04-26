bdac1d08c197f5b89a39e0694f5d35f27dce0730
ventilation fan <146828843+Ent-Dev-116@users.noreply.github.com>
2025-04-26 21:44:15 +0900

    やらかし①　Errorだらけ

--- Sql_Easy.vb
+++ Sql_Easy.vb
@@ -1,7 +1,7 @@
-Public class SqlEasy
+Public Class SqlEasy
     ' SqlEasyクラスは、SQL Serverに接続してSQL文を実行するためのクラスです。
     ' 使い方は、Sqlクラスのインスタンスを作成し、Executeメソッドを呼び出すだけです。 
-    
+
     ' 初期化は接続プロパティを設定するだけでOKです。
     ' また、本文には列の配列を用意しておいてください。引数として使用します。
 
@@ -11,69 +11,142 @@ Public class SqlEasy
     ' Dim dt As DataTable = sql.Get("SELECT column1, column2 FROM table_name", New String() {"column1", "column2"})
 
     ' 接続用の文字列
-    Data_Source As String;
-    User_ID As String;
-    Password As String;
-    Inital_Catalog As String;
+    Private Data_Source As String
+    Private User_ID As String
+    Private Password As String
+    Private Inital_Catalog As String
 
-    Sub New(String As Sourse,
-            String As id,
-            String As Password,
-            String As Database)
-        Me.Data_Source = Sourse;
-        Me.User_ID = id;
-        Me.Password = Password;
-        Me.Inital_Catalog = Database;
+    Sub New(Sourse As String,
+            ID As String,
+            Password As String,
+            Database As String)
+        Me.Data_Source = Sourse
+        Me.User_ID = ID
+        Me.Password = Password
+        Me.Inital_Catalog = Database
     End Sub
 
     ' SQL文を実行するメソッド(set)
-    Sub Set(String As Sql)
-        Dim cn As New SqlClient.SqlConnection
-        cn.ConnectionString = "Data Source=" & this.Data_Source & ";Initial Catalog=" & this.Inital_Catalog & ";User ID=" & this.User_ID & ";Password=" & this.Password & ";"
-        
-        cn.Open()
+    Function SetDB(Sql As String) As Boolean
+        Try
+            Dim cn As New Microsoft.Data.SqlClient.SqlConnection
+            cn.ConnectionString = "Data Source=" & Me.Data_Source & " Initial Catalog=" & Me.Inital_Catalog & " User ID=" & Me.User_ID & " Password=" & Me.Password & " "
 
-        Dim cmd As New SqlClient.SqlCommand
-        cmd.CommandText = Sql
-        cmd.Connection = cn
-        cmd.ExecuteNonQuery()
+            cn.Open()
 
-        cn.Close()
-        cn.Dispose()
-    End Sub
+            Dim cmd As New Microsoft.Data.SqlClient.SqlCommand
+            cmd.CommandText = Sql
+            cmd.Connection = cn
+            cmd.ExecuteNonQuery()
 
-    ' SQL文を実行するメソッド(get)
-    Function Get(String As Sql,
-            String() As Columns) As DataTable
-        Dim cn As New SqlClient.SqlConnection
-        cn.ConnectionString = "Data Source=" & this.Data_Source & ";Initial Catalog=" & this.Inital_Catalog & ";User ID=" & this.User_ID & ";Password=" & this.Password & ";"
-        
-        cn.Open()
-
-        Dim cmd As New SqlClient.SqlCommand
-        cmd.CommandText = Sql
-        cmd.Connection = cn
-
-        Dim dr As SqlClient.SqlDataReader = cmd.ExecuteReader()
-
-        Dim dtData As New DataTable
-        For i As Integer = 0 To Columns.Length - 1
-            dtData.Columns.Add(Columns(i)) 
-        Next i
-
-        While dr.Read()
-            Dim drData As DataRow = dtData.NewRow()
-            For i As Integer = 0 To Columns.Length - 1
-                drData(i) = dr(Columns(i))
-            Next i
-            dtData.Rows.Add(drData)
-        End While
-
-        dr.Close()
-        cn.Close()
-        cn.Dispose()
-
-        Return dtData
+            cn.Close()
+            cn.Dispose()
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return False
+        End Try
     End Function
 
-End class
+    ' SQL文を実行するメソッド(get)
+    Public Function GetDB(Sql As String,
+            Columns As String()) As DataTable
+        Try
+
+            Dim cn As New Microsoft.Data.SqlClient.SqlConnection
+            cn.ConnectionString = "Data Source=" & Me.Data_Source & " Initial Catalog=" & Me.Inital_Catalog & " User ID=" & Me.User_ID & " Password=" & Me.Password & " "
+
+            cn.Open()
+
+            Dim cmd As New Microsoft.Data.SqlClient.SqlCommand
+            cmd.CommandText = Sql
+            cmd.Connection = cn
+
+            Dim dr As Microsoft.Data.SqlClient.SqlDataReader = cmd.ExecuteReader()
+
+            Dim dtData As New DataTable
+            For i As Integer = 0 To Columns.Length - 1
+                dtData.Columns.Add(Columns(i))
+            Next i
+
+            While dr.Read()
+                Dim drData As DataRow = dtData.NewRow()
+                For i As Integer = 0 To Columns.Length - 1
+                    drData(i) = dr(Columns(i))
+                Next i
+                dtData.Rows.Add(drData)
+            End While
+
+            dr.Close()
+            cn.Close()
+            cn.Dispose()
+
+            Return dtData
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return Nothing
+        End Try
+    End Function
+
+    Function SetDB(Sql As String, ErrorText As String) As Boolean
+        Try
+            Dim cn As New Microsoft.Data.SqlClient.SqlConnection
+            cn.ConnectionString = "Data Source=" & Me.Data_Source & " Initial Catalog=" & Me.Inital_Catalog & " User ID=" & Me.User_ID & " Password=" & Me.Password & " "
+
+            cn.Open()
+
+            Dim cmd As New Microsoft.Data.SqlClient.SqlCommand
+            cmd.CommandText = Sql
+            cmd.Connection = cn
+            cmd.ExecuteNonQuery()
+
+            cn.Close()
+            cn.Dispose()
+            Return True
+        Catch ex As Exception
+            MsgBox(ErrorText)
+            Return False
+        End Try
+    End Function
+
+    ' SQL文を実行するメソッド(get)
+    Public Function GetDB(Sql As String,
+            Columns As String(),
+            ErrorText As String) As DataTable
+        Try
+
+            Dim cn As New Microsoft.Data.SqlClient.SqlConnection
+            cn.ConnectionString = "Data Source=" & Me.Data_Source & " Initial Catalog=" & Me.Inital_Catalog & " User ID=" & Me.User_ID & " Password=" & Me.Password & " "
+
+            cn.Open()
+
+            Dim cmd As New Microsoft.Data.SqlClient.SqlCommand
+            cmd.CommandText = Sql
+            cmd.Connection = cn
+
+            Dim dr As Microsoft.Data.SqlClient.SqlDataReader = cmd.ExecuteReader()
+
+            Dim dtData As New DataTable
+            For i As Integer = 0 To Columns.Length - 1
+                dtData.Columns.Add(Columns(i))
+            Next i
+
+            While dr.Read()
+                Dim drData As DataRow = dtData.NewRow()
+                For i As Integer = 0 To Columns.Length - 1
+                    drData(i) = dr(Columns(i))
+                Next i
+                dtData.Rows.Add(drData)
+            End While
+
+            dr.Close()
+            cn.Close()
+            cn.Dispose()
+
+            Return dtData
+        Catch ex As Exception
+            MsgBox(ErrorText)
+            Return Nothing
+        End Try
+    End Function
+End Class
